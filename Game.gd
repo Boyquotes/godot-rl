37178adaf4e11d57ac6963731e158e6134a14bda
dominik johann <dominikjohann@gmail.com>
2021-03-13 20:38:41 +0100

    smaller

--- Game.gd
+++ Game.gd
@@ -8,7 +8,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	OS.set_window_size(Vector2(1280, 720))
+	OS.set_window_size(Vector2(400,300))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
