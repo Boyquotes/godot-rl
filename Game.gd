1df9b85565002a5983f2096fe227c13901c637c8
dominik johann <dominikjohann@gmail.com>
2021-03-14 16:08:40 +0100

    generate map, connect rooms, place doors

--- Game.gd
+++ Game.gd
@@ -5,14 +5,14 @@ extends Node2D
 const TILE_SIZE = 10
 
 const LEVEL_SIZES = [
-	Vector2(80, 80),
+	Vector2(40, 30),
 	Vector2(40, 40),
 	Vector2(50, 50),
 ]
 
 const LEVEL_ROOM_COUNT = [5, 7, 9, 12, 15]
-const MIN_ROOM_DIMENSION = 5
-const MAX_ROOM_DIMENSION = 8
+const MIN_ROOM_DIMENSION = 6
+const MAX_ROOM_DIMENSION = 10
 
 # enum to get tiles by index ---------------------------------------------------
 enum Tile {Player, Stone, Floor, Ladder, Wall, Door}
@@ -61,7 +61,246 @@ func build_level():
 			map[x].append(Tile.Stone)
 			tile_map.set_cell(x, y, Tile.Stone)
 			
+	# set region but keep one tile edge of stone
+	var free_regions = [Rect2(Vector2(2, 2), level_size - Vector2(4, 4))]
+	
+	var num_rooms = LEVEL_ROOM_COUNT[level_num]
+	for i in range(num_rooms):
+		add_room(free_regions)
+		if free_regions.empty():
+			break
+			
+	connect_rooms()
 
+# function to connect existing rooms -------------------------------------------
+
+func connect_rooms():
+	# build an A* graph of areas where we can add corridors
+	
+	# add all stone tiles outside the rooms
+	var stone_graph = AStar.new()
+	var point_id = 0
+	for x in range(level_size.x):
+		for y in range(level_size.y):
+			if map[x][y] == Tile.Stone:
+				stone_graph.add_point(point_id, Vector3(x , y, 0))
+				
+				# connect to left if also stone
+				if x > 0 && map[x - 1][y] == Tile.Stone:
+					var left_point = stone_graph.get_closest_point(Vector3(x - 1, y, 0))
+					stone_graph.connect_points(point_id, left_point)
+					
+				# connect to above if also stone
+				if y > 0 && map[x][y -1] == Tile.Stone:
+					var above_point = stone_graph.get_closest_point(Vector3(x, y - 1, 0))
+					stone_graph.connect_points(point_id, above_point)
+					
+				point_id += 1
+	
+	# build an a* graph of room connections
+	# see if there's a path between all of them
+	
+	var room_graph = AStar.new()
+	point_id = 0
+	for room in rooms:
+		var room_center = room.position + room.size / 2
+		room_graph.add_point(point_id, Vector3(room_center.x, room_center.y, 0))
+		point_id += 1
+	
+	# add random connections until everything is connected
+	
+	while !is_everything_connected(room_graph):
+		add_random_connection(stone_graph, room_graph)
+
+# function that returns true if everything has a connection --------------------
+
+func is_everything_connected(graph):
+	var points = graph.get_points()
+	var start = points.pop_back()
+	# try to get a path for every other point
+	# if this fails, something is not connected
+	for point in points:
+		var path = graph.get_point_path(start, point)
+		if !path:
+			return false
+	
+	return true
+	
+func add_random_connection(stone_graph, room_graph):
+	# pick rooms to connect
+	
+	var start_room_id = get_least_connected_point(room_graph)
+	var end_room_id = get_nearest_unconnected_point(room_graph, start_room_id)
+	
+	# pick door locations
+	var start_position = pick_random_door_location(rooms[start_room_id])
+	var end_position = pick_random_door_location(rooms[end_room_id])
+
+	# find a path to connect the doors to each other
+	var closest_start_point = stone_graph.get_closest_point(start_position)
+	var closest_end_point = stone_graph.get_closest_point(end_position)
+	
+	var path = stone_graph.get_point_path(closest_start_point, closest_end_point)
+	assert(path)
+	
+	# add path to the map
+	
+	set_tile(start_position.x, start_position.y, Tile.Door)
+	set_tile(end_position.x, end_position.y, Tile.Door)
+	
+	for position in path:
+		set_tile(position.x, position.y, Tile.Floor)
+		
+	room_graph.connect_points(start_room_id, end_room_id)
+	
+# helper functions for above ---------------------------------------------------
+
+func get_least_connected_point(graph):
+	var point_ids = graph.get_points()
+	
+	# least will hold the point with least connections seen so far
+	var least
+	# list of all the points with that many connections
+	var tied_for_least = []
+	
+	for point in point_ids:
+		var count = graph.get_point_connections(point).size()
+		if !least || count < least:
+			least = count
+			tied_for_least = [point]
+		elif count == least:
+			tied_for_least.append(point)
+		
+	return tied_for_least[randi() % tied_for_least.size()]
+	
+func get_nearest_unconnected_point(graph, target_point):
+	var target_position = graph.get_point_position(target_point)
+	var point_ids = graph.get_points()
+	
+	var nearest
+	var tied_for_nearest = []
+	
+	for point in point_ids:
+		if point == target_point:
+			continue
+		
+		var path = graph.get_point_path(point, target_point)
+		if path:
+			continue
+		
+		var dist = (graph.get_point_position(point) - target_position).length()
+		if !nearest || dist < nearest:
+			nearest = dist
+			tied_for_nearest = [point]
+		elif dist == nearest:
+			tied_for_nearest.append(point)
+			
+	return tied_for_nearest[randi() % tied_for_nearest.size()]
+
+func pick_random_door_location(room):
+	var options = []
+	
+	# door can be put on any wall
+	
+	# top and bottom walls
+	for x in range(room.position.x + 1, room.end.x - 2):
+		options.append(Vector3(x, room.position.y, 0))
+		options.append(Vector3(x, room.end.y - 1, 0))
+	
+	# left and right walls
+	for y in range(room.position.y + 1, room.end.y - 2):
+		options.append(Vector3(room.position.x, y, 0))
+		options.append(Vector3(room.end.x - 1, y, 0))
+		
+	# TODO: make sure two doors can't spawn adjacent
+	
+	return options[randi() % options.size()]
+
+# function to add a room within free regions -----------------------------------
+
+func add_room(free_regions):
+	var region = free_regions[randi() % free_regions.size()]
+	
+	# go bigger if possible 
+	var size_x = MIN_ROOM_DIMENSION 
+	if region.size.x > MIN_ROOM_DIMENSION:
+		size_x += randi() % int(region.size.x - MIN_ROOM_DIMENSION)
+	
+	var size_y = MIN_ROOM_DIMENSION
+	if region.size.y > MIN_ROOM_DIMENSION:
+		size_y += randi() % int(region.size.y - MIN_ROOM_DIMENSION)
+		
+	# constrain to maximum room size
+	size_x = min(size_x, MAX_ROOM_DIMENSION)
+	size_y = min(size_y, MAX_ROOM_DIMENSION)
+		
+	var start_x = region.position.x
+	if region.size.x > size_x:
+		start_x += randi() % int(region.size.x - size_x)
+		
+	var start_y = region.position.y
+	if region.size.y > size_y:
+		start_y += randi() % int(region.size.y - size_y)
+	
+	var room = Rect2(start_x, start_y, size_x, size_y)
+	rooms.append(room)
+	
+	for x in range(start_x, start_x + size_x):
+		set_tile(x, start_y, Tile.Wall)
+		set_tile(x, start_y + size_y - 1, Tile.Wall)
+		
+	for y in range(start_y + 1, start_y + size_y - 1):
+		set_tile(start_x, y, Tile.Wall)
+		set_tile(start_x + size_x - 1, y, Tile.Wall)
+		
+		for x in range(start_x + 1, start_x + size_x - 1):
+			set_tile(x, y, Tile.Floor)
+			
+	cut_regions(free_regions, room)
+
+func cut_regions(free_regions, region_to_remove):
+	var removal_queue = []
+	var addition_queue = []
+	
+	for region in free_regions:
+		if region.intersects(region_to_remove):
+			removal_queue.append(region)
+			
+			var leftover_left = region_to_remove.position.x - region.position.x - 1
+			var leftover_right = region.end.x - region_to_remove.end.x - 1
+			var leftover_above = region_to_remove.position.y - region.position.y - 1
+			var leftover_below = region.end.y - region_to_remove.end.y - 1
+			
+		
+			if leftover_left >= MIN_ROOM_DIMENSION:
+				addition_queue.append(Rect2(region.position, Vector2(leftover_left, region.size.y)))
+			if leftover_right >= MIN_ROOM_DIMENSION:
+				addition_queue.append(Rect2(Vector2(region_to_remove.end.x + 1, region.position.y), Vector2(leftover_right, region.size.y)))
+			if leftover_above >= MIN_ROOM_DIMENSION:
+				addition_queue.append(Rect2(region.position, Vector2(region.size.x, leftover_above)))
+			if leftover_below >= MIN_ROOM_DIMENSION:
+				addition_queue.append(Rect2(Vector2(region.position.x, region_to_remove.end.y + 1), Vector2(region.size.x, leftover_below)))
+				
+	for region in removal_queue:
+		free_regions.erase(region)
+		
+	for region in addition_queue:
+		free_regions.append(region)
+
+# function to set tiles --------------------------------------------------------
+
+func set_tile(x, y, type):
+	map[x][y] = type
+	tile_map.set_cell(x,y,type)
+	
+	
+	
+	
+	
+	
+	
+	
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
